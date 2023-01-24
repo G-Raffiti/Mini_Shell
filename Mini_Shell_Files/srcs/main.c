bb8e7634402fc57f2bfd37881da0b6e5c6f690df
G-Raffiti <69368335+G-Raffiti@users.noreply.github.com>
2023-01-24 12:55:12 +0100

    main maj archi

--- Mini_Shell_Files/srcs/main.c
+++ Mini_Shell_Files/srcs/main.c
@@ -6,12 +6,14 @@
 /*   By: rbonneva <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 18:13:58 by rbonneva          #+#    #+#             */
-/*   Updated: 2023/01/23 19:53:28 by rbonneva         ###   ########.fr       */
+/*   Updated: 2023/01/24 12:42:39 by rbonneva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include "../incs/mini_shell.h"
+
+int g_exit_code;
 
 t_error parse_input(t_mini_shell *mini_shell)
 {
@@ -20,26 +22,30 @@ t_error parse_input(t_mini_shell *mini_shell)
 	return (SUCCESS);
 }
 
+static void	loop(t_mini_shell *mini_shell)
+{
+	char	*line;
+
+	while (TRUE)
+	{
+		clear_input(mini_shell);
+		line = read_line();
+		if (!line)
+			return ;
+		if (parse_line(mini_shell, line) == ERROR)
+			return ;
+
+	}
+}
+
 int	main(int argc, char **argv, char **env)
 {
 	t_mini_shell	mini_shell;
 
+	printf("Hello Hell !\n");
 	if (new_mini_shell(&mini_shell) == MALLOC_ERROR)
 		exit_malloc(&mini_shell);
 	initialize_mini_shell(&mini_shell, argc, argv, env);
-
-	while (mini_shell)
-	{
-		if (parse_input(&mini_shell) != SUCCESS)
-		{
-			clear_input(mini_shell);
-			print_error(mini_shell);
-		}
-		else
-		{
-			exec_cmds(mini_shell);
-		}
-	}
+	loop(&mini_shell);
 	exit_end_program(&mini_shell);
-	printf("Hello Hell !");
 }
