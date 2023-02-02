078ccbb618f6eafdb09dd24b4f45c8b2b472df7c
aucaland <aurelien.calandrino@outlook.fr>
2023-02-02 05:23:40 +0100

    Update replace_dollars.c

--- Mini_Shell_Files/srcs/parsing/replace_dollars.c
+++ Mini_Shell_Files/srcs/parsing/replace_dollars.c
@@ -11,25 +11,37 @@ int 	valid_id(char c)
 	return (1);
 }
 
-//void	count_and_next_arg(t_cmd *cmds, int *i, )
+int	is_not_alpha(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (0);
+	return (1);
+}
+
+//void	count_arg(char *raw_cmd, int *split_len, int *prev_is_arg, char quote)
+//{
+//
+//
+//}
+
 t_error	split_count(t_cmd *cmds, int *split_len)
 {
-	int		i;
 	char	quote;
 	char	*raw_cmd;
 	int 	prev_is_arg;
+	int	i;
 
 	i = -1;
 	quote = 0;
 	prev_is_arg = 0;
 	raw_cmd = cmds->raw_cmd;
+
 	while (raw_cmd[++i])
 	{
-		if (set_quote_state(raw_cmd[i], &quote) != '\'' && raw_cmd[i] == '$')
+		if (set_quote_state(raw_cmd[i], &quote) != '\'' && raw_cmd[i] == '$' &&
+			is_not_alpha(raw_cmd[i + 1]))
 		{
-			if (raw_cmd[i + 1] >= '0' && raw_cmd[i + 1] <= '9')
-				continue;
-			else if (i != 0 && raw_cmd[i + 1] && valid_id(raw_cmd[i + 1]) && prev_is_arg == 0)
+			if (i != 0 && raw_cmd[i + 1] && valid_id(raw_cmd[i + 1]) && prev_is_arg == 0)
 				*split_len += 2;
 			else if (raw_cmd[i + 1] && valid_id(raw_cmd[i + 1]))
 				*split_len += 1;
@@ -47,6 +59,7 @@ t_error	split_count(t_cmd *cmds, int *split_len)
 		else
 			prev_is_arg = 0;
 	}
+//	count_arg(raw_cmd, split_len, &prev_is_arg, quote);//TODO : REFACTO 40 LIGNEEEEEE
 	if (prev_is_arg == 0)
 		(*split_len)++;
 	if ((*split_len) == 1 && prev_is_arg == 0)
@@ -54,7 +67,51 @@ t_error	split_count(t_cmd *cmds, int *split_len)
 	(*split_len)++;
 	dprintf(2, "SPLIT_LEN : %d\n\n", *split_len);
 	return (SUCCESS);
+}
 
+t_error	fill_split(t_cmd *cmds, char ***splited_raw)
+{
+	char	quote;
+	char	*raw_cmd;
+	int 	prev_is_arg;
+	int	i;
+
+	i = -1;
+	quote = 0;
+	prev_is_arg = 0;
+	raw_cmd = cmds->raw_cmd;
+
+	while (raw_cmd[++i])
+	{
+		if (set_quote_state(raw_cmd[i], &quote) != '\'' && raw_cmd[i] == '$' &&
+			is_not_alpha(raw_cmd[i + 1]))
+		{
+			if (i != 0 && raw_cmd[i + 1] && valid_id(raw_cmd[i + 1]) && prev_is_arg == 0)
+				*split_len += 2;
+			else if (raw_cmd[i + 1] && valid_id(raw_cmd[i + 1]))
+				*split_len += 1;
+			else
+				continue;
+			while (raw_cmd[i + 1] && valid_id(raw_cmd[i + 1]))
+			{
+				prev_is_arg = 1;
+				set_quote_state(raw_cmd[i], &quote);
+				i++;
+				if (raw_cmd[i] == '?')
+					break;
+			}
+		}
+		else
+			prev_is_arg = 0;
+	}
+//	count_arg(raw_cmd, split_len, &prev_is_arg, quote);//TODO : REFACTO 40 LIGNEEEEEE
+	if (prev_is_arg == 0)
+		(*split_len)++;
+	if ((*split_len) == 1 && prev_is_arg == 0)
+		return (ERROR);
+	(*split_len)++;
+	dprintf(2, "SPLIT_LEN : %d\n\n", *split_len);
+	return (SUCCESS);
 }
 
 t_error	replace_dollars(t_mini_shell *ms, t_cmd *cmds)
@@ -69,12 +126,9 @@ t_error	replace_dollars(t_mini_shell *ms, t_cmd *cmds)
 	if (!cmds->raw_cmd || split_count(cmds, &split_len) == 0)
 		return (SUCCESS);
 	splited_raw = ft_calloc(sizeof(char *), split_len);
-
 	if (!splited_raw)
 		return (MALLOC_ERROR);
-
-
+	if (fill_split(cmds, &splited_raw) == MALLOC_ERROR)
+		return (free_split(splited_raw), MALLOC_ERROR);
 	return(0);
-
-
 }
