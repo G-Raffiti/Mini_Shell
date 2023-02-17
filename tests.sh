2746be1f60e9df2279203bd98fc5c7171776ab9a
aucaland <aurelien.calandrino@outlook.fr>
2023-02-17 15:20:25 +0100

    save

--- tests.sh
+++ tests.sh
@@ -5,20 +5,23 @@ testing()
 {
   local CMD="$1"
   # Run the command in bash and capture the exit code and output
-  bash_output=$(eval $CMD 2>&1 | grep -o "lso.*")
+  eval $CMD
   bash_exit_code=$?
+  bash_output=$(eval $CMD 2>&1 | grep -o "$CMD.*")
 
   # Write the exit code and output to a file
-  echo "exit code: $bash_exit_code" > output_bash.txt
-  echo "$bash_output" >> output_bash.txt
+  echo "$bash_output" > output_bash.txt
+  echo "exit code: $bash_exit_code" >> output_bash.txt
 
   # Run the command in your minishell and capture the exit code and output
-  minishell_output=$(echo "$CMD" | ./ms.out | grep -v "Welcome to" | grep -v "ms ")
+  echo "$CMD" | ./ms.out test
   minishell_exit_code=$?
+  minishell_output=$(echo "$CMD" | ./ms.out test | grep -v "Welcome to" | grep -v "ms ")
+
 
   # Write the exit code and output to the same file
-  echo "exit code: $minishell_exit_code" > output_ms.txt
-  echo "$minishell_output" >> output_ms.txt
+  echo "$minishell_output" > output_ms.txt
+  echo "exit code: $minishell_exit_code" >> output_ms.txt
   >> output_diff.txt diff output_ms.txt output_bash.txt
 }
 
