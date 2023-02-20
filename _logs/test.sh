93a8aad243b4df609f23c90923d24d2f94ce778f
aurelien <aurelien.calandrino@outlook.fr>
2023-02-20 21:42:13 +0100

    LEAKS +test.sh
    -leaks in ft_export with dupnoquote fixed
    -fix some other leaks
    -add in test.sh make + import in dir minishell

--- _logs/test.sh
+++ _logs/test.sh
@@ -1,41 +1,38 @@
-
-> all_valgrinds.log
+> __all_valgrinds.log
+cd ..
+make
+cd ./_logs
+rm minishell
+cp ../minishell ./
 
 testing()
 {
     local CMD="$1"
-    VAR="##################################################################################################################################################################################################################"
-    VAR+=$(echo"")
-    VAR+=$(echo"$CMD")
-    STD=$(echo "$CMD" | valgrind --show-leak-kinds=all --leak-check=full --track-fds=yes --verbose --show-mismatched-frees=yes --read-var-info=yes --suppressions=ignore_leaks.txt --log-file=valgrind-out.log ./minishell test)
-    VAR+=$(< valgrind-out.log grep "==" | cat)
-    VAR+=$(echo"$CMD")
-    VAR+=$(echo"")
-    echo "$VAR" > valgrinds.log
-    sed -i '/blocks are still reachable/c\REACHABLE' valgrinds.log
-    sed -i '/== $/c\END' valgrinds.log
-    sed -i '/REACHABLE/,/END/d' valgrinds.log
-    sed -i 's/END$/-/g' valgrinds.log
+    STD=$(echo "$CMD" | valgrind --show-leak-kinds=all --leak-check=full --track-fds=yes --verbose --show-mismatched-frees=yes --read-var-info=yes --suppressions=__ignore_leaks.txt --log-file=__valgrind-out.log ./minishell test)
+    printf "\n################################################################################################################################################################################################################## \n\n CMD -> $CMD \n" > __valgrinds.log
+    < __valgrind-out.log grep "==" | cat >> __valgrinds.log
+    printf "\n CMD -> $CMD \n " >> __valgrinds.log
 
-    cat valgrinds.log >> all_valgrinds.log
+    sed -i '/blocks are still reachable/c\REACHABLE' __valgrinds.log
+    sed -i '/embedded gdbserver/c\SUPPR' __valgrinds.log
+    sed -i '/ THIS PROCESS USING /c\SUPPR' __valgrinds.log
+    sed -i '/WARNING: new redirection conflicts with existing/c\SUPPR' __valgrinds.log
+#    sed -i '/ Open file descriptor /c\OPEN' __valgrinds.log
+#    sed -i '/ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)/c\SUPPR' __valgrinds.log
+    sed -i '/REACHABLE/,/== $/d' __valgrinds.log
+    sed -i '/SUPPR/,/== $/d' __valgrinds.log
+#    sed -i '/OPEN/,/== $/d' __valgrinds.log
+
+    cat __valgrinds.log >> __all_valgrinds.log
 }
 
 counter=0
 while IFS= read -r line; do
-  testing "$line" &
-    # Increment the process counter
-    ((counter++))
-
-    # If we have reached the maximum number of processes, wait for them to finish
-    if [ $counter -eq 4 ]; then
-      wait
-      # Reset the counter
-      counter=0
-    fi
+  testing "$line"
 done < minihell.txt
 
 wait
 
-cat all_valgrinds.log | grep lost > all_lost.log
-cat all_valgrinds.log | grep alid > inv_read.log
-cat all_valgrinds.log | grep SIG > SEGFAULT.log
+cat __all_valgrinds.log | grep lost > __all_lost.log
+cat __all_valgrinds.log | grep alid > __inv_read.log
+cat __all_valgrinds.log | grep SIG > __SEGFAULT.log
