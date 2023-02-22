b13e1e4f45a4ebde2b94d4b244fc3eba96ae742b
aucaland <aurelien.calandrino@outlook.fr>
2023-02-22 12:33:26 +0100

    trying to fix ctrl-V on readline
    pour l'instant marche pas

--- Mini_Shell_Files/srcs/parsing/read_line.c
+++ Mini_Shell_Files/srcs/parsing/read_line.c
@@ -2,11 +2,22 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "../../incs/mini_shell.h"
+#include <termios.h>
+
+void	handle_readline_conflict_keybinding()
+{
+	struct termios term;
+
+	tcgetattr(STDIN_FILENO, &term);
+	term.c_cc[VLNEXT] = _POSIX_VDISABLE;
+	tcsetattr(STDIN_FILENO, TCSANOW, &term);
+}
 
 char	*read_line(void)
 {
 	char	*line;
 
+	//handle_readline_conflict_keybinding();
 	set_interactiv_signals();
 	if (get_exit_code() == 0)
 		line = readline(PROMPT""GREEN"-▶ "WHITE);
